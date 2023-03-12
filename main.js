998825445be225b381e2d9b706255d4b6b71ccc8
Ayeza <ayezabashir442@gmail.com>
2023-03-12 20:16:10 +0500

    frontend pagination

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const pagination_element = document.getElementById('pagination');
 let current_page = 1;
 let rows = 5;
 
-function displayList(items, wrapper, rows_per_page, page) {
+function DisplayList(items, wrapper, rows_per_page, page) {
     wrapper.innerHTML = "",
         page--;
 
@@ -50,4 +50,36 @@ function displayList(items, wrapper, rows_per_page, page) {
     }
 }
 
-displayList(list_items, list_element, rows, current_page);
+function SetupPagination(items, wrapper, rows_per_page) {
+    wrapper.innerHTML = "";
+
+    let page_count = Math.ceil(items.length / rows_per_page);
+
+    for (let i = 1; i < page_count + 1; i++) {
+        let btn = PaginationButtion(i, items);
+        wrapper.appendChild(btn);
+    }
+}
+
+function PaginationButtion(page, items) {
+    let button = document.createElement('button');
+    button.innerText = page;
+
+    if (current_page == page) button.classList.add('active');
+
+    button.addEventListener('click', function () {
+        current_page = page;
+        DisplayList(items, list_element, rows, current_page);
+
+        let current_btn = document.querySelector('.pagenumbers button.active');
+        current_btn.classList.remove('active');
+
+        button.classList.add('active');
+    });
+
+    return button;
+}
+
+DisplayList(list_items, list_element, rows, current_page);
+
+SetupPagination(list_items, pagination_element, rows);
